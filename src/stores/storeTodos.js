f281abd788e0ae0857944c2a6fcd806c3b969442
John Knotts <jhnnyk@gmail.com>
2024-07-31 11:14:21 -0600

    initial store with dummy data

---
+++ src/stores/storeTodos.js
@@ -0,0 +1,51 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useStoreTodos = defineStore('todos', () => {
+  const todos = ref([
+    {
+      id: 'id1',
+      pageId: 'pageId1',
+      name: 'Go Shopping',
+      description: 'lorem ipsum',
+      completed: false,
+    },
+    {
+      id: 'id2',
+      pageId: 'pageId1',
+      name: 'Buy skateboard',
+      description: 'lorem ipsum',
+      completed: false,
+    },
+    {
+      id: 'id3',
+      pageId: 'pageId2',
+      name: 'buy car tires',
+      description: 'lorem ipsum',
+      completed: false,
+    },
+    {
+      id: 'id4',
+      pageId: 'pageId2',
+      name: 'walk the dog',
+      description: 'lorem ipsum',
+      completed: false,
+    },
+  ])
+
+  const pages = ref([
+    {
+      id: 'pageId1',
+      name: 'Errands',
+    },
+    {
+      id: 'pageId2',
+      name: 'Chores',
+    },
+  ])
+
+  return {
+    todos,
+    pages,
+  }
+})
