c8d92c16029ae9f36d3e89de6abeeac7952c949d
John Knotts <jhnnyk@gmail.com>
2024-08-01 16:12:55 -0600

    add isLoading states to signup and login

--- src/stores/storeUsers.js
+++ src/stores/storeUsers.js
@@ -27,33 +27,35 @@ export const useStoreUsers = defineStore('users', () => {
   }
 
   const loginUser = (formData) => {
+    userIsLoading.value = true
     signInWithEmailAndPassword(auth, formData.email, formData.password)
       .then((userCredential) => {
         // Signed in
-        const user = userCredential.user
-        console.log(user)
+        userIsLoading.value = false
       })
       .catch((error) => {
         const errorCode = error.code
         const errorMessage = error.message
         console.log(errorCode, errorMessage)
+        userIsLoading.value = false
       })
   }
 
   const registerUser = (formData) => {
+    userIsLoading.value = true
     createUserWithEmailAndPassword(auth, formData.email, formData.password)
       .then((userCredential) => {
         // Signed up
-        const user = userCredential.user
         updateProfile(auth.currentUser, {
           displayName: formData.name,
         })
-        console.log(user)
+        userIsLoading.value = false
       })
       .catch((error) => {
         const errorCode = error.code
         const errorMessage = error.message
         console.log(errorCode, errorMessage)
+        userIsLoading.value = false
       })
   }
 
