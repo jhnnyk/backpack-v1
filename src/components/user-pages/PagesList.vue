d5ed5eef66e1ed84d94f7f540d5f1491d78be752
John Knotts <jhnnyk@gmail.com>
2024-08-09 09:00:24 -0600

    show todo count for each page

--- src/components/user-pages/PagesList.vue
+++ src/components/user-pages/PagesList.vue
@@ -5,13 +5,17 @@
         <q-icon name="mdi-shopping" />
       </q-item-section>
       <q-item-section class="text-grey-9">{{ page.name }}</q-item-section>
-      <q-item-section side>22</q-item-section>
+      <q-item-section side>{{
+        storeTodos.pageTodos(page.id).length
+      }}</q-item-section>
     </q-item>
   </q-list>
 </template>
 
 <script setup>
 import { useStorePages } from 'src/stores/storePages'
+import { useStoreTodos } from 'src/stores/storeTodos'
 
 const storePages = useStorePages()
+const storeTodos = useStoreTodos()
 </script>
