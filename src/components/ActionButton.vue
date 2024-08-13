ed8fc3b4ff09005dad179e65ffc741fc0e71dd6e
John Knotts <jhnnyk@gmail.com>
2024-08-13 11:06:44 -0600

    different icons

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,7 +1,7 @@
 <template>
   <q-fab color="primary" icon="mdi-plus" direction="left">
-    <q-fab-action icon="mdi-checkbox-marked-circle-plus-outline" />
-    <q-fab-action icon="mdi-file-document-plus-outline" />
+    <q-fab-action icon="mdi-checkbox-marked-circle-outline" />
+    <q-fab-action icon="mdi-note-text-outline" />
     <q-fab-action icon="mdi-format-header-1" />
     <q-fab-action icon="mdi-line-scan" />
   </q-fab>
