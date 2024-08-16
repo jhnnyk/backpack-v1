e054d913ac4245b84da47aad254afb97810121fe
John Knotts <jhnnyk@gmail.com>
2024-08-16 07:40:55 -0600

    don't use titles on text items anymore

--- src/components/content-items/content-item-types/TextItem.vue
+++ src/components/content-items/content-item-types/TextItem.vue
@@ -1,8 +1,5 @@
 <template>
   <q-item-section class="text-grey-9">
-    <q-item-label>
-      <h4 class="text-center q-my-sm">{{ item.title }}</h4>
-    </q-item-label>
     <div v-html="item.description"></div>
   </q-item-section>
 </template>
